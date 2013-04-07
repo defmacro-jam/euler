b4bc283672afcfd73b908de08045fc01c35ee192
Michael Bentley <michael@mycolor.is>
2013-04-07 14:38:38 -0400

    problem 007 (brute force)

---
+++ problem-007.lisp
@@ -0,0 +1,19 @@
+
+;;;; Problem 007
+
+;; State-of-the-art seems to be the Sieve of Atkin, but brute force, I
+;; think, is fine here.
+
+(nth 10000
+     (remove-if #'zerop 
+                (loop for i upto 105000
+                   collecting (if (primep i)
+                                  i
+                                  0))))
+
+;; took 156,250 microseconds (0.156250 seconds) to run.
+;;       12,632 microseconds (0.012632 seconds, 8.08%) of which was spent in GC.
+;; During that period, and with 2 available CPU cores,
+;;      150,998 microseconds (0.150998 seconds) were spent in user mode
+;;        1,240 microseconds (0.001240 seconds) were spent in system mode
+;;  3,515,232 bytes of memory allocated.
