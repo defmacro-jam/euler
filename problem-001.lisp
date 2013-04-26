29912d033acff14294cfbf36371f1118d3161457
Michael Bentley <michael@mycolor.is>
2013-04-26 15:26:25 -0400

    clearer without the extra lines

--- problem-001.lisp
+++ problem-001.lisp
@@ -2,26 +2,20 @@
 
 (defun problem-001 ()
   "Returns the sum of multiples of 3 and 5 that are less than 1000"
-  (- (+ (sum-of-integers-below 1000 
-                               :that-are-multiples-of 3)
-        (sum-of-integers-below 1000 
-                               :that-are-multiples-of 5))
-     (sum-of-integers-below 1000 
-                            :that-are-multiples-of 15)))
+  (- (+ (sum-of-integers-below 1000 :that-are-multiples-of 3)
+        (sum-of-integers-below 1000 :that-are-multiples-of 5))
+     (sum-of-integers-below 1000 :that-are-multiples-of 15)))
 
 
 
 (defun sum-of-integers-below (limit &key (that-are-multiples-of 1))
   "Returns the sum of multiples below limit."
   (let ((mult that-are-multiples-of))
-    (assert (and (integerp limit)
-                 (integerp mult))
+    (assert (and (integerp limit) (integerp mult))
             (limit mult))
     (* mult
-       (/ (* (floor (1- limit)
-                    mult)
-             (1+ (floor (1- limit)
-                        mult)))
+       (/ (* (floor (1- limit) mult)
+             (1+ (floor (1- limit) mult)))
           2))))
 
 
